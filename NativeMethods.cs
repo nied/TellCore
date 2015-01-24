692ddc648ba3ff6914451be1579b4560e499e9c0
Markus Olsson <j.markus.olsson@gmail.com>
2015-01-23 16:56:45 -0800

    Native methods should be internal

--- NativeMethods.cs
+++ NativeMethods.cs
@@ -11,126 +11,126 @@ namespace TellCore
     internal static class NativeMethods
     {
         [DllImport("TelldusCore.dll")]
-        public static extern int tdGetNumberOfDevices();
+        internal static extern int tdGetNumberOfDevices();
 
         [DllImport("TelldusCore.dll")]
-        public static extern int tdGetDeviceId(int value);
-
-        [DllImport("TelldusCore.dll")]
-        [return: MarshalAs(UnmanagedType.CustomMarshaler, MarshalCookie = "out", MarshalTypeRef = typeof(TelldusUtf8Marshaler))]
-        public static extern string tdGetName(int deviceId);
+        internal static extern int tdGetDeviceId(int value);
 
         [DllImport("TelldusCore.dll")]
         [return: MarshalAs(UnmanagedType.CustomMarshaler, MarshalCookie = "out", MarshalTypeRef = typeof(TelldusUtf8Marshaler))]
-        public static extern string tdGetProtocol(int deviceId);
+        internal static extern string tdGetName(int deviceId);
 
         [DllImport("TelldusCore.dll")]
         [return: MarshalAs(UnmanagedType.CustomMarshaler, MarshalCookie = "out", MarshalTypeRef = typeof(TelldusUtf8Marshaler))]
-        public static extern string tdGetModel(int deviceId);
+        internal static extern string tdGetProtocol(int deviceId);
 
         [DllImport("TelldusCore.dll")]
         [return: MarshalAs(UnmanagedType.CustomMarshaler, MarshalCookie = "out", MarshalTypeRef = typeof(TelldusUtf8Marshaler))]
-        public static extern string tdGetDeviceParameter(
+        internal static extern string tdGetModel(int deviceId);
+
+        [DllImport("TelldusCore.dll")]
+        [return: MarshalAs(UnmanagedType.CustomMarshaler, MarshalCookie = "out", MarshalTypeRef = typeof(TelldusUtf8Marshaler))]
+        internal static extern string tdGetDeviceParameter(
             int deviceId,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalCookie = "in", MarshalTypeRef = typeof(TelldusUtf8Marshaler))] string name,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalCookie = "in", MarshalTypeRef = typeof(TelldusUtf8Marshaler))] string defaultValue);
 
         [DllImport("TelldusCore.dll")]
-        public static extern bool tdSetName(
+        internal static extern bool tdSetName(
             int deviceId,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalCookie = "in", MarshalTypeRef = typeof(TelldusUtf8Marshaler))] string name);
 
         [DllImport("TelldusCore.dll")]
-        public static extern bool tdSetProtocol(
+        internal static extern bool tdSetProtocol(
             int deviceId,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalCookie = "in", MarshalTypeRef = typeof(TelldusUtf8Marshaler))] string protocol);
 
         [DllImport("TelldusCore.dll")]
-        public static extern bool tdSetModel(
+        internal static extern bool tdSetModel(
             int deviceId,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalCookie = "in", MarshalTypeRef = typeof(TelldusUtf8Marshaler))] string model);
 
         [DllImport("TelldusCore.dll")]
-        public static extern bool tdSetDeviceParameter(
+        internal static extern bool tdSetDeviceParameter(
             int deviceId,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalCookie = "in", MarshalTypeRef = typeof(TelldusUtf8Marshaler))] string name,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalCookie = "in", MarshalTypeRef = typeof(TelldusUtf8Marshaler))] string value);
 
         [DllImport("TelldusCore.dll")]
-        public static extern int tdAddDevice();
+        internal static extern int tdAddDevice();
 
         [DllImport("TelldusCore.dll")]
-        public static extern bool tdRemoveDevice(int deviceId);
+        internal static extern bool tdRemoveDevice(int deviceId);
 
         [DllImport("TelldusCore.dll")]
-        public static extern int tdMethods(int deviceId, int methodsSupported);
+        internal static extern int tdMethods(int deviceId, int methodsSupported);
 
         [DllImport("TelldusCore.dll")]
-        public static extern int tdTurnOn(int deviceId);
+        internal static extern int tdTurnOn(int deviceId);
 
         [DllImport("TelldusCore.dll")]
-        public static extern int tdTurnOff(int deviceId);
+        internal static extern int tdTurnOff(int deviceId);
 
         [DllImport("TelldusCore.dll")]
-        public static extern int tdBell(int deviceId);
+        internal static extern int tdBell(int deviceId);
 
         [DllImport("TelldusCore.dll")]
-        public static extern int tdDim(int deviceId, char level);
+        internal static extern int tdDim(int deviceId, char level);
 
         [DllImport("TelldusCore.dll")]
-        public static extern int tdExecute(int deviceId);
+        internal static extern int tdExecute(int deviceId);
 
         [DllImport("TelldusCore.dll")]
-        public static extern int tdUp(int deviceId);
+        internal static extern int tdUp(int deviceId);
 
         [DllImport("TelldusCore.dll")]
-        public static extern int tdDown(int deviceId);
+        internal static extern int tdDown(int deviceId);
 
         [DllImport("TelldusCore.dll")]
-        public static extern int tdStop(int deviceId);
+        internal static extern int tdStop(int deviceId);
 
         [DllImport("TelldusCore.dll")]
         [return: MarshalAs(UnmanagedType.CustomMarshaler, MarshalCookie = "out", MarshalTypeRef = typeof(TelldusUtf8Marshaler))]
-        public static extern string tdGetErrorString(int errorNo);
+        internal static extern string tdGetErrorString(int errorNo);
 
         [DllImport("TelldusCore.dll")]
-        public static extern void tdClose();
+        internal static extern void tdClose();
 
         [DllImport("TelldusCore.dll")]
-        public static extern void tdInit();
+        internal static extern void tdInit();
 
         [DllImport("TelldusCore.dll")]
-        public static extern int tdLastSentCommand(int deviceId, int methods);
+        internal static extern int tdLastSentCommand(int deviceId, int methods);
 
         [DllImport("TelldusCore.dll")]
-        public static extern int tdGetDeviceType(int deviceId);
+        internal static extern int tdGetDeviceType(int deviceId);
 
         [DllImport("TelldusCore.dll")]
-        public static extern int tdSendRawCommand(
+        internal static extern int tdSendRawCommand(
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalCookie = "in", MarshalTypeRef = typeof(TelldusUtf8Marshaler))]string command, 
             int reserved);
 
         [DllImport("TelldusCore.dll")]
-        public static extern int tdLearn(int deviceId);
+        internal static extern int tdLearn(int deviceId);
 
         [DllImport("TelldusCore.dll")]
         [return: MarshalAs(UnmanagedType.CustomMarshaler, MarshalCookie = "out", MarshalTypeRef = typeof(TelldusUtf8Marshaler))]
-        public static extern string tdLastSentValue(int deviceId);
+        internal static extern string tdLastSentValue(int deviceId);
         
         [DllImport("TelldusCore.dll")]
-        public static extern void tdReleaseString(IntPtr value);
+        internal static extern void tdReleaseString(IntPtr value);
 
         [DllImport("TelldusCore.dll")]
-        public static extern int tdUnregisterCallback(int eventId);
+        internal static extern int tdUnregisterCallback(int eventId);
 
         [DllImport("TelldusCore.dll")]
-        public static extern int tdRegisterDeviceEvent(Delegate deviceEventFunction, IntPtr context);
+        internal static extern int tdRegisterDeviceEvent(Delegate deviceEventFunction, IntPtr context);
 
         [DllImport("TelldusCore.dll")]
-        public static extern int tdRegisterRawDeviceEvent(Delegate rawListeningFunction, IntPtr context);
+        internal static extern int tdRegisterRawDeviceEvent(Delegate rawListeningFunction, IntPtr context);
 
         [DllImport("TelldusCore.dll")]
-        public static extern int tdRegisterDeviceChangeEvent(Delegate deviceChangeEventFunction, IntPtr context);
+        internal static extern int tdRegisterDeviceChangeEvent(Delegate deviceChangeEventFunction, IntPtr context);
 
         [UnmanagedFunctionPointer(CallingConvention.StdCall)]
         public delegate void EventFunctionDelegate(
